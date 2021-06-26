5095b29618a495818f4d3a40fdc8b1b705eeec0d
桑原雅史 <brucemclarenp4@gmail.com>
2021-06-26 21:58:11 +0900

    cellを構築

--- Swift5TableViewBasic/NextViewController.swift
+++ Swift5TableViewBasic/NextViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class NextViewController: UIViewController {
+    
+    
+    var toDoString = String()
+    @IBOutlet weak var todoLabel: UILabel!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        todoLabel.text = toDoString
     }
     
 
--- Swift5TableViewBasic/ViewController.swift
+++ Swift5TableViewBasic/ViewController.swift
@@ -7,30 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate {
     
     
     
+    @IBOutlet weak var textField: UITextField!
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var textArray = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        textField.delegate = self
         
         
     }
 
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
-//    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = textArray[indexPath.row]
+        cell.imageView!.image = UIImage(named:"checkedImage")
+        
+        return cell
+        
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return view.frame.size.height/6
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textArray.append(textField.text!)
+        textField.resignFirstResponder()
+        textField.text = ""
+        tableView.reloadData()
+        
+//        return true
+    }
+    
 }
 
